f290d0a87651588753cf3f53737d28c0d63e86e3
slscaffidi123abc <54339234+slscaffidi123abc@users.noreply.github.com>
2019-09-27 15:04:20 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MethodHW
+{
+    class Methods
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        public void myBadCode(float param1f, float param2f);
+        {
+          var average = param1f / param2f;
+        Console.WriteLine
+        }
+        
+    {
+        Console.WriteLine("I have got to keep practicing!");
+
+    }
+    
+
+
+                       
+        
+        
+
+        
+    }
+}
